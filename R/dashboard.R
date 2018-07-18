a041c8a4432aaa24497eb38faff050fa5b685891
enguerrandgrx <enguerrand.granoux@epfl.ch>
2018-07-18 17:59:05 +0200

    First commit

---
+++ R/dashboard.R
@@ -0,0 +1,36 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+
+randomplot <- function(n, dist=c("normal", "uniform")){
+  #input validation
+  dist <- match.arg(dist)
+  stopifnot(n < 1e6)
+
+  if(dist == "normal"){
+    hist(rnorm(n))
+  }
+
+  if(dist == "uniform"){
+    hist(runif(n))
+  }
+
+  #return nothing
+  invisible();
+}
